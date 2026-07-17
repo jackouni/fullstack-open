6ea39a6f204d743ab8fd0e94d54fed0c94361b59
Jack Sebben <jacksebben11@hotmail.com>
2026-07-17 11:29:48 -0600

    Final submission for unicafe exercises

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,93 @@
 import { useState } from 'react'
 
+const Title = ({ text }) => (<h1>{text}</h1>)
+
+const Button = ({ text, onClick }) => {
+  return (<button onClick={onClick}>{text}</button>)
+}
+
+const StatisticLine = ({ text, val }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{val}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ stats }) => {
+  if (stats.length === 0) return (<p>No feedback given</p>)
+
+  return (
+    <table>
+      <tbody>
+        {stats.map(stat => {
+          return (
+            <StatisticLine
+            key={stat.text}
+            text={stat.text}
+            val={stat.val}
+            />
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + bad + neutral;
+  const score = good - bad;
+  const scoreAverage = all > 0 ? score / all : "";
+  const percent = (metric) =>  all > 0 ? `${metric / all * 100}%` : "";
+
+  const stats = all === 0 ? [] : [
+    { text: "good", val: good },
+    { text: "neutral", val: neutral },
+    { text: "bad", val: bad },
+    { text: "all", val: all },
+    { text: "average", val: scoreAverage },
+    { text: "positive", val: percent(good) },
+  ];
+
   return (
     <div>
-      code here
+      <div>
+        <Title text="give feedback"/>
+        <Button 
+          text={"good"} 
+          onClick={() => setGood(good + 1)}
+        />
+
+        <Button 
+          text={"neutral"} 
+          onClick={() => setNeutral(neutral + 1)}
+        />
+
+        <Button 
+          text={"bad"} 
+          onClick={() => setBad(bad + 1)}
+        />
+
+        <Button 
+          text={"reset"} 
+          onClick={() => {
+            setBad(0)
+            setGood(0)
+            setNeutral(0)
+          }}
+        />
+      </div>
+
+
+      <div>
+        <Title text={"statistics"}/>
+        <Statistics stats={stats}/>
+      </div>
     </div>
   )
 }
