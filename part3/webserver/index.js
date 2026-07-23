e0a05319d4be4f1d67b22ed14eb09e3ab67649af
Jack Sebben <jacksebben11@hotmail.com>
2026-07-23 16:30:20 -0600

    Part 3 Phonebook assignment started. Solution for exercise step 3.1

--- part3/webserver/index.js
+++ part3/webserver/index.js
@@ -19,6 +19,8 @@ let notes = [
   }
 ];
 
+app.use(express.json())
+
 // Defining Routes:
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -26,6 +28,53 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const noteToDelete = notes.find(note => note.id === id)
+
+  if (noteToDelete) {
+    notes = notes.filter(note => note !== noteToDelete);
+    response.status(204).end()
+  }
 })
 
 // Listen on port 3001:
